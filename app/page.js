eb6b0a3d1a119279603eb4df013e9f4c185b6399
Zeous_Shrey <shrey.sadhukhan21@gmail.com>
2023-09-25 21:22:36 +0530

    Shrey

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className=''>
+      <div className='bg-black-500 text-white h-full w-full text-center hover:text-green-500 cursor-pointer text-2xl '>Ai-chatbot</div>
+    </main>
+  )
+}
